2616e2e73bd73b3059dca75db08d2bf595b0235a
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-06-06 15:25:48 +0100

    Update dbhandler.py

--- dbhandler.py
+++ dbhandler.py
@@ -50,7 +50,7 @@ def daily(ctx):
 	now = time.time()
 	dailytime = now + 86400
 	c.execute('SELECT daily FROM avviebot WHERE userid= %s', (ctx.message.author.id,))
-	if int(c.fetchone()) >= now or c.fetchone() is None:
+	if float(c.fetchone()) >= now or c.fetchone() is None:
 		if "vip" in [y.name.lower() for y in ctx.message.author.roles]:
 			c.execute('UPDATE avviebot SET balance = balance + 200 WHERE userid = %s', (ctx.message.author.id,))
 			c.execute('UPDATE avviebot SET daily = %s WHERE userid = %s', (str(dailytime),ctx.message.author.id,))
