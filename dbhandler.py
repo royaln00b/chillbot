5428c5f20605eabc8d3741f51819b1db9078a753
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-06-06 20:29:46 +0100

    Apparently not
    This isn't going to plan apparently

--- dbhandler.py
+++ dbhandler.py
@@ -56,7 +56,7 @@ def daily(ctx):
 	data2 = data1.replace(")","")
 	data3 = data2.replace(",","")
 	floated = int(data3)
-	if floated >= now or floated == 0:
+	if floated >= now:
 		if "vip" in [y.name.lower() for y in ctx.message.author.roles]:
 			c.execute('UPDATE avviebot SET balance = balance + 200 WHERE userid = %s', (ctx.message.author.id,))
 			c.execute('UPDATE avviebot SET daily = %d WHERE userid = %s', (dailytime,ctx.message.author.id,))
@@ -65,6 +65,9 @@ def daily(ctx):
 			c.execute('UPDATE avviebot SET balance = balance + 100 WHERE userid = %s', (ctx.message.author.id,))
 			c.execute('UPDATE avviebot SET daily = %d WHERE userid = %s', (dailytime,ctx.message.author.id,))
 			return True
+	elif floated == 0:
+		c.execute('UPDATE avviebot SET daily = %d WHERE userid = %s', (now,ctx.message.author.id,))
+		
 	else:
 		return False
 
