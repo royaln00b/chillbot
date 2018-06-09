8578d4416aa61289034bf969002ab6cd44cc3024
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-06-09 14:25:43 +0100

    Added first 2 Achievements

--- avviebotpy.py
+++ avviebotpy.py
@@ -1259,6 +1259,14 @@ async def on_message(message,):
                                         elif randdrop == 10:
                                             dbhandler.randomdrop(message)
                                             await bot.send_message(message.channel,message.author.display_name+" has received 100<:Coin:439199818447978508> as a random drop!")
+                                        elif dbhandler.checkbal(message) == "500000" and not "baller" in [y.name.lower() for y in message.author.roles]:
+                                            await bot.add_roles(message.author,discord.utils.get(message.server.roles, name="Baller"))
+                                            embed = discord.Embed(title="Achievement! | "+message.author.display_name,description=message.author.display_name+" earnt the achievement :moneybag:Baller:moneybag: !",colour=0xEE82EE)
+                                            await bot.say(embed=embed)
+                                        elif dbhandler.checkbal(message) == "1000000" and not "millionaire" in [y.name.lower() for y in message.author.roles]:
+                                            await bot.add_roles(message.author,discord.utils.get(message.server.roles, name="Millionaire"))
+                                            embed = discord.Embed(title="Achievement! | "+message.author.display_name,description=message.author.display_name+" earnt the achievement :moneybag:Millionaire:moneybag: !",colour=0xEE82EE)
+                                            await bot.say(embed=embed)
     await bot.process_commands(message)
        
     
