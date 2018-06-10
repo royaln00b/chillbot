9457546f406be4de058096f8bd350d430502ca5c
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-06-10 17:08:05 +0100

    Update avviebotpy.py

--- avviebotpy.py
+++ avviebotpy.py
@@ -742,7 +742,7 @@ async def profile(ctx,*,member:discord.Member=None):
 		achievements = achievements + "\n:dart:2 Achievements:dart:"
 	if achieves >= 5:
 		achievements = achievements + "\n:medal:5 Achievements:medal:"
-	embed=discord.Embed(title = member.name , description=member.display_name+" has "+str(roles)+", the highest role they have is "+toprole+"\n"+member.display_name+" joined discord at : "+str(member.created_at)[:10]+"\nThey joined this server at : "+str(member.joined_at)[:10]+"\nCurrently playing : "+str(member.game)+"\nCurrent status : "+str(member.status)+"\n\n"+achievements+"\n\n**Balance :** " +dbhandler.whoisbalance(member), colour = 0xEE82EE)
+	embed=discord.Embed(title = member.name , description=member.display_name+" has {0}".format(roles)+", the highest role they have is "+toprole+"\n"+member.display_name+" joined discord at : "+str(member.created_at)[:10]+"\nThey joined this server at : "+str(member.joined_at)[:10]+"\nCurrently playing : "+str(member.game)+"\nCurrent status : "+str(member.status)+"\n\n"+achievements+"\n\n**Balance :** " +dbhandler.whoisbalance(member), colour = 0xEE82EE)
 	embed.set_thumbnail(url = member.avatar_url)
 	embed.set_image(url = str(dbhandler.whoislevel(member)))
 	embed.set_footer(text="Requested by : "+ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
