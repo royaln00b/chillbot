6846b2d2987a2fa0c34f1c2af89ac64bd4582ef0
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-10-01 20:39:17 +0100

    Update avviebotpy.py

--- avviebotpy.py
+++ avviebotpy.py
@@ -279,7 +279,7 @@ async def on_message(message):
 
 @bot.event
 async def on_command_error(error, args):
-	await bot.say("An error occured : "+error+" due to "+args)
+	await bot.say("An error occured : "+str(error)+" due to "+str(args))
 	
 
 """
