a04c50959f8ce961dbe76bdf6d11afd42214c0bf
royaln00b <37339403+royaln00b@users.noreply.github.com>
2019-02-26 21:29:17 +0000

    Update to -mute command, still in testing phase

--- avviebotpy.py
+++ avviebotpy.py
@@ -152,7 +152,8 @@ async def mute(ctx,member:discord.Member,*,reason="None"):
 				await bot.add_roles(member,discord.utils.get(ctx.message.server.roles, name="Muted"))
 				await bot.say(embed=embed)
 			else:
-				await bot.create_role(server=ctx.message.server.name,name="Muted",permissions=discord.Permissions(send_messages=0))
+				# !!!!!!!!!!!!! FIX ME !!!!!!!!!!
+				await bot.create_role(server=ctx.message.server,name="Muted",permissions=discord.Permissions(send_messages=0))
 				embed=discord.Embed(title="Success",description="I have created a new role called `Muted` which will allow you to mute people.",colour=0xFFC600)
 				await bot.say(embed=embed)
 		else:
